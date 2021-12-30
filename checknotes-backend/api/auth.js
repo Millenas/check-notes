efc105f4f40411ecfa753dcc994cf58a261a7ab7
Gabriel Ramos Nascimento <gabrielramosnascimento195@gmail.com>
2021-12-30 16:47:19 -0400

    alterações

--- checknotes-backend/api/auth.js
+++ checknotes-backend/api/auth.js
@@ -8,7 +8,7 @@ module.exports = app => {
             return res.status(400).send('Dados incompletos')
         }
 
-        const user = await app.db('taskers')
+        const user = await app.db('users')
             .whereRaw("LOWER(email) = LOWER(?)", req.body.email)
             .first()
 
--- checknotes-backend/migrations/20211230005040_create_table_tasks.js
+++ checknotes-backend/migrations/20211230005040_create_table_tasks.js
@@ -3,11 +3,10 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('tasks', table => {
         table.increments('id').primary()
         table.string('desc').notNull()
-        table.boolean('status').notNull()
         table.datetime('estimateAt')
         table.datetime('doneAt')
-        table.integer('taskId').references('id')
-            .inTable('taskers').notNull()
+        table.integer('userId').references('id')
+            .inTable('users').notNull()
     })
 };
 
